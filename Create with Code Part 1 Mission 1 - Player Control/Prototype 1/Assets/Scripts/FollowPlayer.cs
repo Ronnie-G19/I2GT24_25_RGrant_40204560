ef54b71c93c044774c6603f8cbe53a0a455361cb
Ronnie-G19 <rgrant18@qub.ac.uk>
2024-09-22 22:32:31 +0100

    Prototype1 - Camera Offset for FollowPlayer

--- Create with Code Part 1 Mission 1 - Player Control/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Create with Code Part 1 Mission 1 - Player Control/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
 
     public GameObject player;
+    private Vector3 offset = new Vector3(0, 8, -10);
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +15,9 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = player.transform.position;
+        // Offset the camera behind the player by adding to the player's position
+        transform.position = player.transform.position + offset;
     }
 }
