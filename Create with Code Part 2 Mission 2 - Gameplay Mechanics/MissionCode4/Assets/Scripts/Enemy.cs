3a46ea9aaa9b88136b803ed1506502bf0b758773
Ronnie-G19 <rgrant18@qub.ac.uk>
2024-11-09 16:10:37 +0000

    missionCode4_GeneratingPowerUps_SpawningMoreEnemies

--- Create with Code Part 2 Mission 2 - Gameplay Mechanics/MissionCode4/Assets/Scripts/Enemy.cs
+++ Create with Code Part 2 Mission 2 - Gameplay Mechanics/MissionCode4/Assets/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 3.0f;
+    public float speed = 2.5f;
     private Rigidbody enemyRb;
     private GameObject player;
     // Start is called before the first frame update
@@ -21,5 +21,12 @@ public class Enemy : MonoBehaviour
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
 
         enemyRb.AddForce(lookDirection* speed);
+
+        if(transform.position.y < -10)
+        {
+
+            Destroy(gameObject);
+
+        }
     }
 }
