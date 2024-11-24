f92be57ccb5ace0e9d7923f5c45c399cf9039838
Ronnie-G19 <rgrant18@qub.ac.uk>
2024-11-24 14:53:53 +0000

    AppleFortUpdate_TimerDone_ActivatesOnStartClick_LevelReLoadAtEnd

--- Project/AppleFort/Assets/Scripts/Start Button.cs
+++ Project/AppleFort/Assets/Scripts/Start Button.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using TMPro.Examples;
 
 public class StartButton : MonoBehaviour
 {
     private Button button;
     private GameManager gameManager;
+    private Timer timer;
+    
     
 
     // Start is called before the first frame update
@@ -14,6 +18,7 @@ public class StartButton : MonoBehaviour
     {
         button = GetComponent<Button>();    
         gameManager = GameObject.Find("Game Manager"). GetComponent<GameManager>();
+        timer = GameObject.Find("Canvas").GetComponent<Timer>();
         button.onClick.AddListener(StartGame);
     }
 
@@ -24,7 +29,9 @@ public class StartButton : MonoBehaviour
     }
     void StartGame()
     {
+        
         Debug.Log(button.gameObject.name + "was clicked");
         gameManager.StartGame();
+        timer.startTimer = true;
     }
 }
--- Project/AppleFort/Assets/Scripts/Timer.cs
+++ Project/AppleFort/Assets/Scripts/Timer.cs
@@ -6,18 +6,27 @@ using TMPro.Examples;
 
 public class Timer : MonoBehaviour
 {
+    public bool startTimer;
     [SerializeField] TextMeshProUGUI timerText;
     float elapsedTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //startTimer = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(startTimer == true) 
+        { 
         elapsedTime += Time.deltaTime;
-        timerText.text = elapsedTime.ToString();
+        int minutes = Mathf.FloorToInt(elapsedTime/60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        int milliseconds = Mathf.FloorToInt((elapsedTime % 1) * 100);
+
+        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliseconds);
+        }
     }
 }
